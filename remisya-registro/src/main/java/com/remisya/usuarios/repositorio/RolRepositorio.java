7f33f11f5f6343c2fea94e5e9797be44cea1d228
FabricioMatias <fabricio.toso@mi.unc.edu.ar>
2023-02-22 20:01:05 -0300

    Descartes

--- remisya-registro/src/main/java/com/remisya/usuarios/repositorio/RolRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.remisya.usuarios.repositorio;
-
-import com.remisya.usuarios.modelo.Rol;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RolRepositorio extends JpaRepository<Rol,Long> {
-    
-}
